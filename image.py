81ee83e37376254036f57dabb56742bbc411ffc5
Shishir-Ashok <shishirashok17@gmail.com>
2019-11-16 14:06:09 +0530

    code corrections

--- image.py
+++ image.py
@@ -1,48 +1,24 @@
 # import the necessary packages
-from PIL import Image
 from itertools import permutations
+from PIL import Image
 import pytesseract
 import argparse
-import requests
 import enchant
 import time
 import cv2
 import os
 import re
-
-
-def findmeaning():
-
-	app_id = '<app_id>'
-	app_key = '<app_key>'
-	language = 'en'
-
-	for i in final:
-		url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/'  + language + '/'  + i.lower()
-		#url Normalized frequency
-		urlFR = 'https://od-api.oxforddictionaries.com:443/api/v2/stats/frequency/word/'  + language + '/?corpus=nmc&lemma=' + i.lower()
-		r = requests.get(url, headers = {'app_id' : app_id, 'app_key' : app_key})
-		response = r.json()
-		print(i.upper(),":\n")
-		for n in range (10):
-			try:
-				print("[{}]".format(n), response['results'][n]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-			except IndexError:
-				break
-			except KeyError:
-				continue
-
  
 start_time = time.time()
 
-
-
-
 dictionary = enchant.Dict("en_US")
-
-
 pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
+
 ap = argparse.ArgumentParser()
+group = ap.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true",
+	help="Display count of words")
+group.add_argument("-q", "--quiet", action="store_true")
 ap.add_argument("-i", "--image", required=True,
 	help="Path to input image to be OCR'd")
 ap.add_argument("-p", "--preprocess", type=str, default="thresh",
@@ -76,38 +52,37 @@ cv2.imwrite(filename, gray)
 
 text = pytesseract.image_to_string(Image.open(filename), lang='eng')
 os.remove(filename)
-print(text)
 
 listOfText = text.split()
 # listOfText = ['OR','RUN','JAU','SE','AS','DEO','CE','AR','IG','NTI','INV','LY','PAN','OFF','BOO','TI','ATE','CA','VI','TTA']
-
-
 perms = []
 for i in range(2,5):
 	perms.append([''.join(p) for p in permutations(listOfText,i)])
 
 perms = sum(perms,[])
-print("Length of perms : ",len(perms))
-
 final = []
-
 for i in perms:
 	if(dictionary.check(i)):
 		final.append(i)
-print("LIST : ",listOfText)
 final.sort(key=len)
-print("The result : \n",final)
 
-# word_id = 'Shanghai'
+if args.verbose:
+	print("Data in image : ",text)
+	print("Length of perms : ",len(perms))
+	print("Number of words found : ",len(final))
+	print("List of words based on the number of letters :")
+	for word in final:
+		print("{} : {}".format(len(word),word))
 
-
-
-
-
+if args.quiet:
+	print("List of words :")
+	for word in final:
+		print(word, end=", ")
 
 # show the output images
-print("--- %s seconds ---" % (time.time() - start_time))
 if args.showoutput:
 	cv2.imshow("Image", image)
 	cv2.imshow("Output", gray)
 	cv2.waitKey(0)
+
+print("Processing time\n--- %s seconds ---" % (time.time() - start_time))
